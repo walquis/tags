36e99500010fcbfdd4ba5d3941bbddb0e799a10d
jarretthosey <jarrett.hosey3@gmail.com>
2020-11-24 10:31:50 -0600

    third

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
+from tag import Tag  # After app_init(), which sets db for the model
+from flask import request, redirect, render_template, g
+from IPython import embed
+from datetime import datetime
+from config import load_config, app_init
 import sys
 from os.path import join
 sys.path.insert(1, sys.path[0])
-from config import load_config, app_init
-from datetime import datetime
-from IPython import embed
-from flask import request, redirect
 
 app = app_init(__name__)
 
-from tag import Tag # After app_init(), which sets db for the model
 
 cfg = load_config(join(app.root_path, '../shared/config.yml'))
 
+
 @app.route('/', methods=['GET'])
 def show_tags():
-    tags = Tag.all()
-    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
-    form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
+    # Flask.g: a way to pass var to a template
+    g.setdefault('image', cfg['awesome_image'])
     #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (cfg['awesome_image'],tags_html, form_html)
+
+    return "<h1>The Ultimate MOST Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (cfg['awesome_image'],tags_html, form_html)
+
+    # Note tags=Tag.all() ... another way to pass var to a Jinja2 template
+    return render_template('index.html', tags=Tag.all())
+
+@app.route('/home', mehtods=['GET'])
+def show_home():
+    return '<div class="topnav"><a class="active" href="#home">Home</a><a href="#news">News</a><a href="#contact">Contact</a><a href="#about">About</a></div><h1>home</h1><p>this is home test a;ldja;ldjfa;lkdsjf;aldjf;alkjdsf;lajdf</p>'
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
